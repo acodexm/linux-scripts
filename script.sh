2f59217bbddcd461606073db4f41ffffc6b5b962
acodexm <adamkx7x@gmail.com>
2018-10-19 00:56:44 +0200

    added basic development script

---
+++ script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y git npm nodejs openjdk-11-jdk
+
+#Mongo
+echo "installing MongoDB"
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+
+
+#Docker
+echo "installing Docker"
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker $USER
+
+
