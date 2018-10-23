170d0c599019b84155dc459ae1d41377d73df9d7
acodexm <adamkx7x@gmail.com>
2018-10-23 10:50:02 +0200

    added JAVA_HOME and reboot prompt

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y git npm nodejs openjdk-11-jdk
+echo `\nJAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"\nexport JAVA_HOME` | sudo tee /etc/environment
 
 #Mongo
 echo "installing MongoDB"
@@ -48,3 +49,13 @@ sudo chmod 0755 ~/Templates/script.sh
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
+
+
+#Finish
+echo "Script finished! Do you want to REBOOT SYSTEM??"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) echo "Reboot system"; reboot; break;;
+        No )  echo "Exit script"; exit;;
+    esac
+done
